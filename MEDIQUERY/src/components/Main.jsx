4d9aa3bfc56f384cf9b1c91745ca5ba94481f50a
Kranthi Prasad <kranthichintu917751@gmail.com>
2025-07-13 15:58:22 +0530

    created logo

---
+++ MEDIQUERY/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      <div className="search bg-[url(https://i.pinimg.com/736x/53/9c/3c/539c3c25f93825de8c1a633775123c1d.jpg)] bg-no-repeat bg-center bg-cover bg-top-14">
+      <div className="searchbar">
+        <input type="search" name="" id="" className='px-4 py-2 w-64 rounded-md border border-gray-300 focus:outline-none ring-2 ring-blue-500 h-100'/>
+
+      </div>
+        
+        
+      </div>
+    </div>
+  )
+}
+
+export default Main
---
+++ MEDIQUERY/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+
+        <nav className='h-30 w-full flex justify-between bg-blue-200'>
+            <div className="upside ">
+                <img src="public/mediquerylogo.png" alt="" className='h-30 w-40 bg-auto rounded-full' />
+                </div>
+                <div className="loginbtns gap-5 flex mt-6 mr-8 ">
+                    <button className='bg-blue-500 text-white hover:font-extrabold cursor-pointer text-1xl rounded-full w-30 h-15'>Login</button>
+                    <button className='bg-blue-500 text-white hover:font-extrabold cursor-pointer w-30  h-15 rounded-full'>SignUp</button>
+                </div>
+            
+        </nav>
+
+    )
+}
+
+export default Navbar
