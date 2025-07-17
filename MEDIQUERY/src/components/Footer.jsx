7297fcb3fe29e0f2011e2244a2267d3cd64873df
Kranthi Prasad <kranthichintu917751@gmail.com>
2025-07-17 16:43:07 +0530

    commit

--- MEDIQUERY/src/components/Footer.jsx
+++ MEDIQUERY/src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='flex mt-6 h-15 bg-orange-500'>
-            <img src="public/information.png" alt="" className=' h-7 mt-3 rounded-full ml-5'/>
+        <div className='flex justify-center mt-6 h-15 bg-orange-500'>
+            <img src="public/information.png" alt="" className=' h-7 mt-3.5 rounded-full ml-5'/>
             <h2 className='font-bold text-center mt-4 ml-0.5'>MediQuery helps you check medicine uses and timings. It’s just for info — not medical advice. Always ask your doctor if needed.</h2>
         </div>
     )
