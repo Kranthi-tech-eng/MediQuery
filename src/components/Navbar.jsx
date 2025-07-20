7a4c74ddd9932d9aa813d5373a3556a1c920e995
Kranthi Prasad <kranthichintu917751@gmail.com>
2025-07-20 13:31:22 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
 
         <nav className='h-30 w-full justify-center sm:flex sm:justify-between bg-blue-200'>
             <div className="upside">
-                <img src="public/logo1.png" alt="" className='w-60 h-15 mx-auto rounded-full sm:h-auto sm:w-60 sm:bg-auto sm:rounded-full sm:mt-5 sm:ml-5' />
+                <img src="/logo1.png" alt="" className='w-60 h-15 mx-auto rounded-full sm:h-auto sm:w-60 sm:bg-auto sm:rounded-full sm:mt-5 sm:ml-5' />
             </div>
             <div className="loginbtns gap-2 sm:gap-5 justify-end flex mt-2 sm:mt-6 sm:mr-8 mr-4">
 
